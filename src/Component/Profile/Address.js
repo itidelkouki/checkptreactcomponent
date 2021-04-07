c0497ac0c3ca69d1fc80dd9aff469b49bea6cba3
ASUS <aatidelkouki@gmail.com>
2021-04-07 15:36:23 +0100

    important

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ComponentThree = () =>{
+    return(
+
+
+    <h2>I'm From Tunisia, i live in Rades, Ben Arous</h2>
+
+
+)
+}
+export default ComponentThree;
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ComponentTwo = () =>{
+    return(
+
+
+    <h2>Hello! My name is Itidel Kouki</h2>
+
+
+)
+}
+export default ComponentTwo;
